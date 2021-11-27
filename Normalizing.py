a6bdb7cfb5cbfbdf78a2a346bd7e8f7ed7d457d0
John <averagehero2@gmail.com>
2021-11-27 01:59:52 -0500

    Added NaiveBayes support
    tier 3 part 1

--- Normalizing.py
+++ Normalizing.py
@@ -15,7 +15,8 @@ def normalize(arg: str) -> str:
     postr = PorterStemmer()
     wnl = WordNetLemmatizer()
     stopWords = stopwords.words('english')
-
+    stopWords.append('’')  # special chars pick up by crawler
+    stopWords.append('‘')
     text = word_tokenize(arg)
     # reverse iteration to aid in deletion of stopwords
     for i, word in reversed(list(enumerate(text))):
@@ -24,10 +25,10 @@ def normalize(arg: str) -> str:
                 # don't replace -
                 continue
             text[i] = text[i].replace(member, " ")
-        if word in stopWords:
+        if word.lower() in stopWords:
             del text[i]
         else:
-            text[i] = wnl.lemmatize(postr.stem(text[i]))
+            text[i] = wnl.lemmatize(postr.stem(text[i].lower()))
 
     return " ".join(text)
 
@@ -60,10 +61,39 @@ def countListOfWords(arg: list) -> list:
     # print(Counter([arg[i].split() for i in range(len(arg))]))
     return wordOccurrence
 
+from gensim.models import Word2Vec
+
+def tryingGensim():
+    listofAllArticles = [csvWriter.readArticleData("acuterheumaticarthritis")[0], csvWriter.readArticleData("acuterheumaticarthritis")[1]]
+    # listofAllArticles = listofAllArticles.split(" ")
+    [tokenized_text, tokens2] = word_tokenize(listofAllArticles[0]), word_tokenize(listofAllArticles[1])
+    test = []
+    test.append(tokenized_text)
+    test.append(tokens2)
+    # print(tokenized_text)
+    model = Word2Vec(test)
+    # for word in listofAllArticles:
+    #     print(word)
+    # print(listofAllArticles)
+    # vector = model.wv['dog']  # numpy vector of word
+    print(model.wv.key_to_index)
+    sims = model.wv.most_similar('of', topn=10)
+    print(sims)
+    # # print(listofAllArticles)
+    # w2v_model = Word2Vec()
+    # w2v_model.build_vocab(listofAllArticles)
+    # for eachWord in w2v_model.wv.key_to_index:
+    #     print(eachWord)
+    # w2v_model.train(listofAllArticles, total_examples=w2v_model.corpus_count, epochs=w2v_model.epochs)
+    # w2v_model.init_sims(replace=True)
+    # w2v_model.
+    # print(w2v_model.wv.most_similar(positive=["clinical"]))
+
 
 if __name__ == '__main__':
-    test = csvWriter.readArticleData("acute rheumatic arthritis")
-    normalizeList(test)
+    # test = csvWriter.readArticleData("acute rheumatic arthritis")
+    # normalizeList(test)
     # countListOfWords(test)
-    print(countWords("im gonna spend all day applying for jobs"))
+    # print(countWords("im gonna spend all day applying for jobs"))
+    tryingGensim()
     # print(test)
--- csvWriter.py
+++ csvWriter.py
@@ -3,48 +3,48 @@ import sys
 from os import path
 
 
-def writeToCsv(article: list, disease_name: str):
+def writeToCsv(article: list, disease_name: str, demo=False) -> bool:
     """
 
     :param article: the article information with url at [0] and article contents as [1]
     :param disease_name: the name of the disease
+    :param demo: writes data to a file that can safely be deleted at any time
     :return: True if successfully written. False is nothing was there to write.
     """
-    disease_name = disease_name.replace(" ", "")
+    disease_name = 'demo' if demo else disease_name.replace(" ", "")
+
     if not article:
         # note: this shouldn't ever happen. exists just in case.
         print(f"No text found to write", file=sys.stderr)
         return False
     with open(f'{disease_name}.csv', 'a', newline="", encoding='utf-8') as f:
         writer = csv.writer(f)
-        # article[1] = article[1].replace(",", "").replace("(", "").replace(")", "").replace("\n", " ")
         article[1] = article[1].replace("\n", " ")
         writer.writerow(article)
-        f.close()
-    print('write complete')
+    print('write complete', end="")
     return True
 
 
-def writeHeader(disease_name):
+def writeHeader(disease_name: str, demo=False) -> bool:
     """
-
+    :param demo: writes data to a file that can safely be deleted at any time
     :param disease_name: the name of the disease
     :return: bool True if successful write. False if already exists.
     """
-    disease_name = disease_name.replace(" ", "")+'.csv'
+    disease_name = 'demo' if demo else disease_name.replace(" ", "")
     # determine if file is already there
     # if it is, no need to write header
-    if path.isfile(disease_name):
+    if path.isfile(disease_name + ".csv"):
         # file & header info is already there.
         return False
     header = ['Link location', 'Article Data', 'Normalized Article', 'BoW\'d Article']
-    with open(disease_name, 'w', newline="", encoding='utf-8') as f:
+    with open(f'{disease_name}.csv', 'w', newline="", encoding='utf-8') as f:
         writer = csv.writer(f)
         writer.writerow(header)
     return True
 
 
-def readArticleData(disease_name) -> list:
+def readArticleData(disease_name: str) -> list:
     """
     :param disease_name: the name of the disease
     :return: string of article contents
@@ -55,8 +55,29 @@ def readArticleData(disease_name) -> list:
         csv_reader = csv.reader(f, delimiter=',')
         next(csv_reader)  # skip over header
         for i, eachRow in enumerate(csv_reader):  # csv_reader uses a generator. cannot be indexed
-            nonnormalizedArticles.append(eachRow[1])
+            nonnormalizedArticles.append(eachRow[2])
+    if not nonnormalizedArticles:
+        raise AttributeError("No data was read")
     return nonnormalizedArticles
+
+
+def readOneArticle(disease_name: str) -> list:
+    """
+    :param disease_name: the name of the disease
+    :return: string of article contents
+    """
+    nonnormalizedArticles = []
+    disease_name = disease_name.replace(" ", "")+'.csv'
+    with open(disease_name, 'r', encoding='utf-8') as f:
+        csv_reader = csv.reader(f, delimiter=',')
+        next(csv_reader)  # skip over header
+        for i, eachRow in enumerate(csv_reader):  # csv_reader uses a generator. cannot be indexed
+            return list(eachRow[1])
+            # for eachword in eachRow[1]:
+            #     nonnormalizedArticles.append(eachword)
+            # return nonnormalizedArticles
+    return []
+
 
 if __name__ == '__main__':
     # tmp = soulCrawler.Crawler("diease, lyme", "1949-1980")  # yields 4 results, all fail
